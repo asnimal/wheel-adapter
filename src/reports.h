ee3e7265c33516ea5e22a153a298269288139468
asnimal <asnimal@gmail.com>
2026-06-15 21:27:48 +0200

    Update reports.h

--- src/reports.h
+++ src/reports.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-// G29 HID report (Lo que va a la PS5)
+// G29 HID report (Estructura final hacia la PS5 - INTACTA)
 typedef struct __attribute__((packed)) {
     uint8_t lx;
     uint8_t ly;
@@ -33,14 +33,27 @@ typedef struct __attribute__((packed)) {
     uint8_t whatever2[13];
 } g29_report_t;
 
-// Estructura híbrida para el G25 (Modo extendido real)
+// Driving Force HID report (Estructura base original - INTACTA)
 typedef struct __attribute__((packed)) {
-    uint16_t wheel : 14;
-    uint32_t buttons : 18;
+    uint32_t wheel : 10;
+    uint32_t cross : 1;
+    uint32_t square : 1;
+    uint32_t circle : 1;
+    uint32_t triangle : 1;
+    uint32_t R1 : 1;
+    uint32_t L1 : 1;
+    uint32_t R2 : 1;
+    uint32_t L2 : 1;
+    uint32_t select : 1;
+    uint32_t start : 1;
+    uint32_t R3 : 1;
+    uint32_t L3 : 1;
+    uint32_t whatever : 2;
+    uint8_t y;
+    uint32_t hat : 4;
+    uint32_t whatever2 : 4;
     uint8_t throttle;
     uint8_t brake;
-    uint8_t clutch;
-    uint8_t dpad;
-} g25_report_t;
+} df_report_t;
 
 #endif
