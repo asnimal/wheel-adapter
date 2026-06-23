f25428d7b97c14c59760e0c2355025f70da87035
asnimal <asnimal@gmail.com>
2026-06-23 17:53:38 +0200

    Update descriptors.c

--- src/descriptors.c
+++ src/descriptors.c
@@ -50,15 +50,15 @@ uint8_t const desc_hid_report[] = {
     0x65, 0x00,        //   Unit (None)
     0x05, 0x09,        //   Usage Page (Button)
     0x19, 0x01,        //   Usage Minimum (0x01)
-    0x29, 0x15,        //   Usage Maximum (0x15)  <-- 21 Botones
+    0x29, 0x0E,        //   Usage Maximum (0x0E)  <-- EXACTAMENTE 14 BOTONES
     0x15, 0x00,        //   Logical Minimum (0)
     0x25, 0x01,        //   Logical Maximum (1)
     0x75, 0x01,        //   Report Size (1)
-    0x95, 0x15,        //   Report Count (21)     <-- 21 Botones
+    0x95, 0x0E,        //   Report Count (14)     <-- EXACTAMENTE 14 BOTONES
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x06, 0x00, 0xFF,  //   Usage Page (Vendor Defined 0xFF00)
     0x09, 0x20,        //   Usage (0x20)
-    0x75, 0x07,        //   Report Size (7)       <-- Relleno de 7 bits
+    0x75, 0x06,        //   Report Size (6)       <-- RELLENO ORIGINAL DE 6 BITS
     0x95, 0x01,        //   Report Count (1)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
