e83c0360ea834a9d811170ca0ea28609aaadb015
asnimal <asnimal@gmail.com>
2026-06-23 14:27:51 +0200

    Update descriptors.c

--- src/descriptors.c
+++ src/descriptors.c
@@ -1,7 +1,5 @@
 #include <stdint.h>
-
 #include "tusb.h"
-
 #include "reports.h"
 
 // G29
@@ -16,23 +14,20 @@ tusb_desc_device_t const desc_device = {
     .bDeviceSubClass = 0x00,
     .bDeviceProtocol = 0x00,
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
-
     .idVendor = USB_VID,
     .idProduct = USB_PID,
     .bcdDevice = 0x0100,
-
     .iManufacturer = 0x01,
     .iProduct = 0x02,
     .iSerialNumber = 0x00,
-
     .bNumConfigurations = 0x01
 };
 
-// Descriptor de reporte modificado para dar soporte a 25 botones
+// G29
 uint8_t const desc_hid_report[] = {
-    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
-    0x09, 0x04,        // Usage (Joystick)
-    0xA1, 0x01,        // Collection (Application)
+    0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
+    0x09, 0x04,        //   Usage (Joystick)
+    0xA1, 0x01,        //   Collection (Application)
     0x85, 0x01,        //   Report ID (1)
     0x09, 0x30,        //   Usage (X)
     0x09, 0x31,        //   Usage (Y)
@@ -55,15 +50,15 @@ uint8_t const desc_hid_report[] = {
     0x65, 0x00,        //   Unit (None)
     0x05, 0x09,        //   Usage Page (Button)
     0x19, 0x01,        //   Usage Minimum (0x01)
-    0x29, 0x19,        //   Usage Maximum (25) -> Modificado a 25 botones (0x19)
+    0x29, 0x15,        //   Usage Maximum (0x15)  <-- 21 Botones
     0x15, 0x00,        //   Logical Minimum (0)
     0x25, 0x01,        //   Logical Maximum (1)
     0x75, 0x01,        //   Report Size (1)
-    0x95, 0x19,        //   Report Count (25) -> Modificado a 25 botones (0x19)
+    0x95, 0x15,        //   Report Count (21)     <-- 21 Botones
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x06, 0x00, 0xFF,  //   Usage Page (Vendor Defined 0xFF00)
     0x09, 0x20,        //   Usage (0x20)
-    0x75, 0x03,        //   Report Size (3) -> Reducido a 3 bits para mantener la alineación total de 32 bits
+    0x75, 0x07,        //   Report Size (7)       <-- ¡CAMBIADO DE 0x06 A 0x07!
     0x95, 0x01,        //   Report Count (1)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
@@ -114,14 +109,16 @@ uint8_t const desc_hid_report[] = {
 #define EPNUM_HID_OUT 0x03
 
 uint8_t const desc_configuration[] = {
+    // Config number, interface count, string index, total length, attribute, power in mA
     TUD_CONFIG_DESCRIPTOR(1, 1, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_SELF_POWERED, 200),
+    // Interface number, string index, protocol, report descriptor len, EP OUT address, EP IN address, size & polling interval
     TUD_HID_INOUT_DESCRIPTOR(0, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID_OUT, EPNUM_HID_IN, CFG_TUD_HID_EP_BUFSIZE, 5)
 };
 
 char const* string_desc_arr[] = {
-    (const char[]){ 0x09, 0x04 },
-    "Logitech",
-    "G29 Driving Force Racing Wheel",
+    (const char[]){ 0x09, 0x04 },      // 0: is supported language is English (0x0409)
+    "Logitech",                        // 1: Manufacturer
+    "G29 Driving Force Racing Wheel",  // 2: Product
 };
 
 uint8_t const* tud_descriptor_device_cb(void) {
@@ -140,7 +137,6 @@ static uint16_t _desc_str[32];
 
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     uint8_t chr_count;
-
     if (index == 0) {
         memcpy(&_desc_str[1], string_desc_arr[0], 2);
         chr_count = 1;
